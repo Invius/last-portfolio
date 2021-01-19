36500e567c18b35f6c25ddb10b02928f38d6e9ff
unknown <marco.fernandes.inf10@hotmail.com>
2021-01-19 23:47:18 +0000

    Updates

---
+++ precache-manifest.7bb568f9795cdf0d0343c72ce50413e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ad8cff77c8bd11563d82e34b8689085",
+    "url": "./index.html"
+  },
+  {
+    "revision": "474521082c9ae957ee26",
+    "url": "./static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "f1caf3d40d8bd72a4634",
+    "url": "./static/css/main.93e4647b.chunk.css"
+  },
+  {
+    "revision": "474521082c9ae957ee26",
+    "url": "./static/js/2.3dd65fe5.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.3dd65fe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1caf3d40d8bd72a4634",
+    "url": "./static/js/main.3da19f83.chunk.js"
+  },
+  {
+    "revision": "e0cd6202f6ac3f0e5d02",
+    "url": "./static/js/runtime-main.46322405.js"
+  },
+  {
+    "revision": "92d20dd00ee7cdc7dc9b778757dfb542",
+    "url": "./static/media/me.92d20dd0.png"
+  }
+]);
